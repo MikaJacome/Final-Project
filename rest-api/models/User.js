c01bf7adcd09209d859ab095787036fcc5a9a7a7
Carla Micaela Jacome <mika_jacome@hotmail.com>
2023-02-05 02:28:00 +0100

    First API functions User Auth

---
+++ rest-api/models/User.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        username:{
+            type: String,
+            require: true,
+            min:3,
+            max:20,
+            unique:true,
+        },
+
+        email:{
+            type: String,
+            required: true,
+            max:50,
+            unique: true,
+        },
+
+        password:{
+            type: String,
+            required: true,
+            min: 6,
+        },
+
+        profilePicture:{
+            type:String,
+            default:"",
+        },
+
+        coverPicture:{
+            type: String,
+            default:"",
+        },
+
+        followers:{
+            type: Array,
+            default:[],
+        },
+
+        following:{
+            type:Array,
+            default:[],
+        },
+
+        isAdmin:{
+            type: Boolean,
+            default: false,
+        },
+        desc:{
+            type: String,
+            max:50
+        },
+        travel:{
+            type: String,
+            max: 50,
+        },
+        from:{
+            type: String,
+            max: 50,
+        },
+        arrives:{
+            type: Date,
+            require: true,
+        }
+    },
+    {timestamps:true}
+);
+
+module.exports = mongoose.model("User", UserSchema);
