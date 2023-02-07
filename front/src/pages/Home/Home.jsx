7477f57122b0e52aad56ea6f076f67445b07bd9c
Carla Micaela Jacome <mika_jacome@hotmail.com>
2023-02-07 22:23:08 +0100

    Fix errors and create feed and side

---
+++ front/src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import Topbar from "../topbar/Topbar";
+import Leftbar from "../../components/Leftbar/Leftbar";
+import Feed from "../feed/feed";
+
+export default function Home(){
+    return(
+        <>
+            <Topbar/>
+            <div className="homeContainer">
+                
+                <Leftbar/>
+                
+                <Feed/>
+
+            </div>
+        </>
+    )
+}
