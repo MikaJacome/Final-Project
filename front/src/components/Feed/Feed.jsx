e0556cc8c92726e628582f812fd30ce58114570a
Carla Micaela Jacome <mika_jacome@hotmail.com>
2023-02-10 22:45:37 +0100

    fix error travel detail not fund

--- front/src/components/Feed/Feed.jsx
+++ front/src/components/Feed/Feed.jsx
@@ -1,7 +1,7 @@
 import Journey from "../Journey/Journey"
 import JourneyDetail from "../journeyDetail/JournyeDetail"
 import CreateTravel from "../Travel/CreateTravel"
-import TravelDetail from "../../travelDetail/TravelDetail"
+import TravelDetail from "../travelDetail/TravelDetail"
 import "./feed.css"
 
 export default function Feed(){
